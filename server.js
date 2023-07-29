d09a9574259d8420ff03825b11e844782c13a93a
Muhammad51264 <muh.freijat@gmail.com>
2023-07-29 20:49:46 +0300

    backend

---
+++ server.js
@@ -0,0 +1,23 @@
+//Import Modules
+const express = require("express");
+const cors=require("cors");
+const helmet = require("helmet");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const mongoose = require("mongoose");
+
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+
+
+
+
+
+
+app.listen(8080, () => {
+    console.log("Server listening on port 8080");
+})
