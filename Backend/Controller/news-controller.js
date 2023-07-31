70c3544e748da44c5d983aeec4aa50dd088f6a0d
mohammed saeed <mohammed.f.zain@gmail.com>
2023-07-31 11:38:27 +0300

    userController Done

--- Backend/Controller/news-controller.js
+++ Backend/Controller/news-controller.js
@@ -1,12 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
-router.get("/", function (req, res) {
-  res.send("hi");
-});
-module.exports = router;
-=======
 
 router.get("/", function(req, res,){
     res.send("hi");
@@ -17,4 +11,3 @@ router.get("/", function(req, res,){
 
 
 module.exports = router;
->>>>>>> 7c3973f816bc2c79a7a3d4355cafdcae1bbad118
--- Backend/Controller/users-controller.js
+++ Backend/Controller/users-controller.js
@@ -1,14 +1,65 @@
-const express = require('express');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../Models/Users"); // Make sure to adjust the path if needed
+const dotenv = require("dotenv");
+dotenv.config({path : "../config.env"});
+
 const router = express.Router();
 
+// Route for user registration
+router.post("/register", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-router.get("/", function(req, res,){ 
-    res.send("hi");
-    
-})
+    // Check if the username or email already exists
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return res.status(409).json({ message: "Username or email already in use." });
+    }
 
+    // Create a new user instance and save it to the database
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+    await newUser.save();
 
+    return res.status(201).json({ message: "User registered successfully." });
+  } catch (error) {
+    console.error("Error during user registration:", error);
+    res.status(500).json({ message: "An error occurred during user registration." });
+  }
+});
 
+// Route for user login
+router.post("/login", async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
+    // Check if the username exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "Invalid credentials." });
+    }
+
+    // Compare the provided password with the hashed password in the database
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Invalid credentials." });
+    }
+
+    // Create a JWT token and send it to the client
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h", // Token expires in 1 hour
+    });
+
+    return res.json({ token });
+  } catch (error) {
+    console.error("Error during user login:", error);
+    res.status(500).json({ message: "An error occurred during user login." });
+  }
+});
 
 module.exports = router;
