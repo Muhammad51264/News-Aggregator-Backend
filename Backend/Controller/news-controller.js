6b3f04358cdf6c0129dfdd43c949bb9c3fae75d1
nadeen adel <nadeenadel.alirr@gmail.com>
2023-07-31 01:01:56 -0800

    save

--- Backend/Controller/news-controller.js
+++ Backend/Controller/news-controller.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const router = express.Router();
 
-<<<<<<< HEAD
-router.get("/", function (req, res) {
-  res.send("hi");
-});
-module.exports = router;
-=======
+const News = require("../Models/News"); // Import the News model
+const Agencies = require("../Models/Agencies"); // Import the Agencies model
 
-router.get("/", function(req, res,){
-    res.send("hi");
-})
+router.get("/news/:agencyName", async (req, res,)=>{
+try{
+  const agencyName = req.params.agencyName;
+
+  const agency = await Agencies.findOne({ name: agencyName });
+  if (!agency) {
+    return res.status(404).json({ error: "Agency not found" });
+  }
+  const newsItems = await News.find({ publisher: agencyName });
+   res.json(newsItems);
+}
+  catch (err) {
+  console.error(err);
+  res.status(500).json({ error: "Internal Server Error" });
+}
+
+    // res.send("hi");
+
+});
 
 
 //get method   response  all news [{}]
 
 
 module.exports = router;
->>>>>>> 7c3973f816bc2c79a7a3d4355cafdcae1bbad118
