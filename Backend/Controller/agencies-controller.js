4d97046b13b9eb2960a2f0e19b7b102284257b66
Muhammad51264 <muh.freijat@gmail.com>
2023-07-31 11:54:22 +0300

    agency-controller

--- Backend/Controller/agencies-controller.js
+++ Backend/Controller/agencies-controller.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Agencies= require("../Models/Agencies");
 const jwt = require("jsonwebtoken");
-
+const News = require("../Models/News");
 
 //Get all Agencies
 router.get("/", async function(req, res,){
@@ -31,6 +31,46 @@ router.post("/register", async function(req, res){
         return res.json({"error": err});
     }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+router.post("/post", async function(req, res){
+    const agency = req.body;
+    try {
+    const foundAgency=await Agencies.findOne({email : agency.email});
+    if (foundAgency){
+        return res.json({"Error": "Email already registered"});
+    }
+    await Agencies.create(agency);
+    console.log("agencies created successfully");
+    return res.json({"status": "Success"});
+    } catch(err){
+        console.log(err);
+        return res.json({"error": err});
+    }
+});
+
+
 
 
 
